77d8beddf52f0ed338b95679830510b0178059d1
姜懿 <375480856@qq.com>
2019-10-05 14:32:22 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace GW2MapGetTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 81);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(675, 455);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(693, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 524);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "增加";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(305, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "减少";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(483, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(565, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(448, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "错误";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(530, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "完成";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(612, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "项目";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(647, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(352, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(41, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "重置";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(88, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "自动获取";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(148, 52);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(41, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "重置";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(84, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(11, 12);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(101, 52);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(41, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "减少";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(41, 52);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(37, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "增加";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(10, 57);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 12);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "层级";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(201, 57);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 12);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "X轴";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 543);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GW2MapGetTool
+{
+    public partial class Form1 : Form
+    {
+        public int t = Properties.Settings.Default.数据;
+        public int tx = Properties.Settings.Default.层级数据;
+        public bool autoget = Properties.Settings.Default.autoget;
+        public int[] 层级X数目 = { 0,3, 5, 10, 20, 40, 79, 157 };
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            checkBox1.Checked = Properties.Settings.Default.autoget;
+            label1.Text = t.ToString();
+            label2.Text = "0";
+            label3.Text = "0";
+            label7.Text = "0";
+            label8.Text = tx.ToString();
+        }
+        int 完成数 = 0;
+        int 项目数 = 0;
+        int 错误数 = 0;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.autoget&& !timer1.Enabled)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                if (timer1.Enabled)
+                {
+                    timer1.Enabled = false;
+                }
+            }
+            xiazai();
+        }
+
+        public void xiazai()
+        {
+             完成数 = 0;
+             项目数 = 0;
+             错误数 = 0;
+            flowLayoutPanel1.Controls.Clear();
+            if (!Directory.Exists(Application.StartupPath + "/1/" + tx+"/" + t))
+            {
+                Directory.CreateDirectory(Application.StartupPath + "/1/" + tx + "/" + t);
+                Application.DoEvents();
+            }
+            {
+                //7层0-148,157, 
+                //6层0-74,79, 
+                //5层0-37,40,
+                //4层0-18,20 
+                //3层0-9-10,
+                //2层0-4-5,
+                //1层0-2-3
+                
+                for (int i = 0; i < 层级X数目[tx]; i++)
+                {
+                    Button button = new Button
+                    {
+                        Text = t.ToString()+"-"+i.ToString(),
+                        Size = new Size(58, 20)
+                    };
+                    flowLayoutPanel1.Controls.Add(button);
+                    Thread thread = new Thread(new ParameterizedThreadStart(delegate { Start(t,i, button); }))
+                    {
+                        IsBackground = true
+                    };
+                    thread.Start();
+                    项目数++;
+                    Application.DoEvents();
+                    
+                }
+            }
+        }
+
+        private void Start(int ii,int iie, Button button)
+        {
+
+            System.Threading.Thread.Sleep(300);
+            if (!File.Exists(Application.StartupPath + "/1/" + tx + "/" + ii + "/" + iie + ".jpg") || File.ReadAllBytes(Application.StartupPath + "/1/" + tx + "/" + ii + "/" + iie + ".jpg").Length < 2)
+            {
+                var wc2 = new WebClient();
+                
+                try
+                {
+                    Uri dws = new Uri("http://tiles.guildwars2.com/1/1/"+tx+"/" + ii + "/" + iie + ".jpg");
+                    wc2.DownloadFile(dws, Application.StartupPath + "/1/" + tx + "/" + ii + "/" + iie + ".jpg");
+                    button.BackColor = Color.Green;
+                    textBox1.AppendText(tx.ToString()+"-"+ii.ToString() + "-" + iie.ToString() + "完成\r\n");
+                    完成数++;
+                }
+                catch (Exception e)
+                {
+                    //string dw= e.Message;
+                    //MessageBox.Show(e.Message);
+                    //textBox1.AppendText(iie.ToString()+"失败" + dw + "\r\n");
+                    textBox1.AppendText(tx.ToString() + "-" + ii.ToString() + "-" + iie.ToString() + "失败\r\n");
+                    button.BackColor = Color.Red;
+                    错误数++;
+                    //throw;
+                }
+                finally
+                {
+                    wc2.Dispose();
+                    
+                }
+            }
+            else
+            {
+                button.BackColor = Color.Green;
+                textBox1.AppendText(tx.ToString() + "-" + ii.ToString() + "-" + iie.ToString() + "已存在\r\n");
+                完成数++;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (t< 149)
+            {
+                t++;
+            }
+            else
+            {
+                textBox1.AppendText(t.ToString()+ "最大值了\r\n");
+            }
+            label1.Text = t.ToString();
+            Properties.Settings.Default.数据 = t;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (t > 0)
+            {
+                t--;
+            }
+            else
+            {
+                textBox1.AppendText(t.ToString() + "最大值了\r\n");
+            }
+            label1.Text = t.ToString();
+            Properties.Settings.Default.数据 = t;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            label8.Text = tx.ToString();
+            label3.Text = 完成数.ToString();
+            label2.Text = 错误数.ToString();
+            label7.Text = 项目数.ToString();
+            if (完成数 == 项目数 && 错误数 ==0 && 项目数 > 0)
+            {
+                
+                if (t <149)
+                {
+                    t++;
+                    label1.Text = t.ToString();
+                    Properties.Settings.Default.数据 = t;
+                    Properties.Settings.Default.Save();
+                    xiazai();
+                }
+                else
+                {
+                    textBox1.AppendText("全部完成\r\n");
+                    timer1.Stop();
+                }
+
+            }
+            if (完成数+ 错误数 == 项目数 && 错误数 > 0 && 完成数 > 0)
+            {
+                xiazai();
+            }
+            else
+            {
+                if (错误数 == 项目数&& 项目数 == 层级X数目[tx])
+                {
+                    if (Directory.Exists(Application.StartupPath + "/1/" + tx + "/" + t))
+                    {
+                        Directory.Delete(Application.StartupPath + "/1/" + tx + "/" + t);
+                    }
+                    if (tx < 7)
+                    {
+                        tx++;
+                        Properties.Settings.Default.层级数据 = tx;
+                        Properties.Settings.Default.Save();
+                        t = 0;
+                        xiazai();
+                    }
+                    else
+                    {
+                        textBox1.AppendText("全部完成\r\n");
+                        timer1.Stop();
+                    }
+
+                }
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            t = 0;
+            label1.Text = t.ToString();
+            Properties.Settings.Default.数据 = t;
+            Properties.Settings.Default.Save();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.autoget = checkBox1.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            tx = 1;
+            label8.Text = tx.ToString();
+            Properties.Settings.Default.层级数据 = tx;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            if (tx<7)
+            {
+                tx++;
+                label8.Text = tx.ToString();
+                Properties.Settings.Default.层级数据 = tx;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            if (tx > 1)
+            {
+                tx--;
+                label8.Text = tx.ToString();
+                Properties.Settings.Default.层级数据 = tx;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
